6c0ac969abc9a3a747d82770ea716706006bcd67
e.nyavro <e.nyavro@eastbanctech.ru>
2014-02-21 15:07:41 +0700

    Removed redundand controller

--- src/main/java/net/nyavro/spring/social/signinmvc/controller/SignUpController.java
+++ src/main/java/net/nyavro/spring/social/signinmvc/controller/SignUpController.java
@@ -11,17 +11,9 @@ public class SignUpController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SignUpController.class);
 
-    /**
-     * Redirects request forward to the registration page. This hack is required because
-     * there is no way to set the sign in url to the SocialAuthenticationFilter class.
-     * Another option is to move registration page to to url '/signup' but I did not
-     * want to do that because that url looks a bit ugly to me.
-     * @return
-     */
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public String redirectRequestToRegistrationPage() {
         LOGGER.debug("Redirecting request to registration page.");
-
         return "redirect:/user/register";
     }
 
